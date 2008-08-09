8af28809222a2af04250adac9faf16660c314b84
davidorme (David Orme) <d.orme@imperial.ac.uk>
2008-08-09 11:38:15 +0000

    made row and column names consistent for single variable data frames
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caic@34 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/clade.stats.R
+++ pkg/R/clade.stats.R
@@ -1,32 +1,39 @@
 "clade.stats" <-
-function(dataf, phyl, fun, ..., tips=FALSE){
+function(dat, phy, fun, ..., tips=FALSE){
 
 	fun <- match.fun(fun)
 
-	if(!is.data.frame(dataf)) stop("Requires data block to be in a dataframe")
+	if(!is.data.frame(dat)) stop("Requires data block to be in a data frame")
 	
-	if(class(phyl) != "phylo") stop("Phylogeny required")
+	if(class(phy) != "phylo") stop("Phylogeny required")
 
-	name.check <- match(phyl$tip.label, rownames(dataf))
+	name.check <- match(phy$tip.label, rownames(dat))
 	
 	if(any(is.na(name.check))){
 		
-		warning("Some tips in phylogeny	not present in data frame: aborting and returning missing tips.")
-		return(phyl$tip.label[is.na(name.check)])
+		warning("Some tips in phylogeny not present in data frame: aborting and returning missing tips.")
+		return(phy$tip.label[is.na(name.check)])
 	
 	} else {
 	
-		clades <- clade.members.list(phyl, tips=tips, tip.labels=TRUE)
+		clades <- clade.members.list(phy, tips=tips, tip.labels=TRUE)
 		
 		
 		# get the rownames 
-		dataf.names <- rownames(dataf)
+		dat.names <- rownames(dat)
 		# ditch non-numeric columns
-		dataf <- dataf[,unlist(lapply(dataf, is.numeric)), drop=FALSE]
+		dat <- dat[,unlist(lapply(dat, is.numeric)), drop=FALSE]
 
 		# ugly line that computes 'fun' for each numeric column in each subset of tips defined by internal nodes
-		cstats <- t(sapply(clades, function(x){apply(dataf[match(x, dataf.names),,drop=FALSE], 2, fun, ...)}, simplify=TRUE))
-		return(as.data.frame(cstats))
+		cstats <- t(sapply(clades, function(x){apply(dat[match(x, dat.names),,drop=FALSE], 2, fun, ...)}, simplify=TRUE))
+		
+		# cope with the way that sapply deals with column vectors
+		if(dim(dat)[2] == 1)  cstats <- t(cstats) 
+        
+        cstats <- as.data.frame(cstats)
+        rownames(cstats) <- names(clades)
+        colnames(cstats) <- colnames(dat)
+		return(cstats)
 	}
 	
 }
