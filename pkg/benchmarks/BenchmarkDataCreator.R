e8d2a20eaef9204e0a54160cac40393bd685c6c7
davidorme (David Orme) <d.orme@imperial.ac.uk>
2008-08-10 20:14:48 +0000

    Updating versions of CAIC, MacroCAIC and FUSCO benchmark datasets and the outputs of CAIC and MacroCAIC benchmark runs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caic@36 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/benchmarks/BenchmarkDataCreator.R
+++ pkg/benchmarks/BenchmarkDataCreator.R
@@ -12,14 +12,25 @@ contVar <- matrix(c(1,0.5,0.25,0.5,1,0,0.25,0,1), ncol=3)
 discTraits <- list(binFact=matrix(c(0,0.05,0.05,0), ncol=2, dimnames=list(c("A","B"), c("A","B"))),
                    triFact=matrix(c(0,0.05,0,0.05,0,0.05,0,0.05,0), ncol=3, dimnames=list(c("A","B","C"), c("A","B","C"))))
 
-BENCH <- growTree(b=1, halt=200,
-                  ct.start=contTraits, ct.change=c(1,2,2), ct.var=contVar,
-                  dt.rates=discTraits, extend.proportion=1, grain=Inf)
+# get a simulation which avoids  short branch lengths at polytomies
+discard <- TRUE
+
+while(discard){
+    BENCH <- growTree(b=0.1, halt=200,
+                      ct.start=contTraits, ct.change=c(0,0,0), ct.var=contVar,
+                      dt.rates=discTraits, extend.proportion=1, grain=Inf)
 
 
-# make seven internal polytomies...
-collapseNodes <- with( BENCH, sort(edge.length[edge[,2] > Nnode +1]))[7]
-BENCHPoly <- align.tips(di2multi(BENCH, collapseNodes))
+    # make seven internal polytomies...
+    collapseNodes <- with( BENCH, sort(edge.length[edge[,2] > Nnode +1]))[7]
+    BENCHPoly <- align.tips(di2multi(BENCH, collapseNodes))
+
+    # check that none of the polytomies have descendents on a branch less than 1
+    nChild <- table(BENCHPoly$edge[,1])
+    polyParent <- as.numeric(names(nChild)[nChild > 2])
+    discard <- any(BENCHPoly$edge.length[which(BENCHPoly$edge[,1] %in% polyParent)] <= 1)
+}
+
 
 write.tree(BENCH, file="BenchTreeDi.tre")
 write.tree(BENCHPoly, file="BenchTreePoly.tre")
@@ -37,28 +48,102 @@ write.caic(BENCHPoly, filebase="BenchTreePoly")
 BenchData <- with(BENCH, cbind(ct.data, dt.data[,-1])) # drop the node labels from dt.data
 BenchData <- BenchData[BenchData$node <= 200,] # drop the internal node data
 
-# - variable that has no variance at two polytomies of the polytomous tree
-#   simply by making all descendents of a polytomy share the same value as the start fr that trait
-polyTab <- data.frame(brtime=branching.times(BENCHPoly), nDesc=table(BENCHPoly$edge[,1]))
-polyTab <- subset(polyTab, nDesc.Freq > 2)
-polyNoVar <- as.numeric(as.character(polyTab[order(polyTab$brtime), 2][1:2]))
+# - variable that has no variance at some polytomies of the polytomous tree
+#   simply by making all descendents of a polytomy share the same value
+#   which is the mean at the node of the values for all descendants (_not_ phylogenetic mean!)
+#   Avoid the polytomies that are too basal...
 
-clMat <- clade.matrix(BENCHPoly)
-polyDesc <- clMat$clade.matrix[polyNoVar,]
-polyDesc <- apply(polyDesc, 2, sum)
-BenchData$polyNoVar <- ifelse(polyDesc, 9, BenchData$contExp1) 
+BenchData$contExp1NoVar <- BenchData$contExp1
+BenchData <- BenchData[order(BenchData$node),]
 
-# CAIC needs alphabetic order
+rownames(BenchData) <- BenchData$node
+nodeVals <- clade.stats(BenchData[,"contExp1", drop=FALSE], BENCHPoly, mean)
+Desc <- clade.members.list(BENCHPoly)
+nDesc <- sapply(Desc, length)
+nDesc <- nDesc[as.character(polyParent)]
+nDesc <- nDesc[nDesc < 20]
+
+for(nd in seq(along=nDesc)){
+    currDesc <- Desc[[names(nDesc)[nd]]]
+    BenchData$contExp1NoVar[currDesc] <- nodeVals[names(nDesc)[nd], "contExp1"]    
+}
+
+
+# Testing handling of missing data for each variable - random 5% in each
+BenchData$contRespNA <- ifelse(runif(200) < 0.05, NA, BenchData$contResp)
+BenchData$contExp1NA <- ifelse(runif(200) < 0.05, NA, BenchData$contExp1)
+BenchData$contExp2NA <- ifelse(runif(200) < 0.05, NA, BenchData$contExp2)
+BenchData$binFactNA <- BenchData$binFact
+BenchData$binFactNA[runif(200) < 0.05] <- NA
+BenchData$triFactNA <- BenchData$triFact
+BenchData$triFactNA[runif(200) < 0.05] <- NA
+
+# CAIC wants categoricals as the last columns in the file.
+BenchData <- BenchData[,c(1:4, 7:10, 5:6, 11:12)]
+
+# CAIC also needs alphabetic order..
 CAICData <- BenchData[order(as.character(BenchData$node)),]
 
-# also needs factors as numerics ..
+# and  factors as numerics ..
 CAICData$binFact <- unclass(CAICData$binFact)
 CAICData$triFact <- unclass(CAICData$triFact)
+CAICData$binFactNA <- unclass(CAICData$binFactNA)
+CAICData$triFactNA <- unclass(CAICData$triFactNA)
 
-# ...and as the last columns in the file.
-CAICData <- CAICData[,c(1:4,7,5,6)]
+write.table(CAICData, file="BenchCAIC.dat", quote=FALSE, sep="\t", row.names=FALSE, na="-9")
 
-write.table(CAICData, file="BenchCAIC.dat", quote=FALSE, sep="\t", row.names=FALSE)
-write.table(BenchData, file="BenchData.txt", quote=FALSE, sep="\t", row.names=FALSE)
+# now create a file of species richnesses at tips for testing MacroCAIC
+# same format and order as the .phyl but with nSpp in place of CAIC code.
+BenchRich <- scan("BenchTreeDi.phyl", what="character")
+
+# no pattern with a variable, but at least follows the expected broken stick!
+brks <- sample(1:4999, 199)
+nSpp <- diff(c(0, sort(brks),5000))
+
+BenchRich[seq(1,399, by=2)] <- nSpp
+cat(BenchRich, file="BenchTreeDi.rich", sep="\n")
+cat(BenchRich, file="BenchTreePoly.rich", sep="\n")
+
+# now insert into the data frame for R
+BenchRich <- matrix(BenchRich, ncol=2, byrow=TRUE)
+BenchRich <- BenchRich[match(BenchData$node, BenchRich[,2]),]
+BenchData$SppRich <- as.numeric(BenchRich[,1])
 
 save(BenchData, BENCH, BENCHPoly, file="CAIC_Benchmark.Rda")
+write.table(BenchData, file="BenchData.txt", quote=FALSE, sep="\t", row.names=FALSE)
+
+# output files for analysis in Fusco
+FuscoDi <- write.tree(BENCH, multi.line=FALSE)
+FuscoDi <- gsub(":[0-9\\.]+", "", FuscoDi) # get rid of the branch lengths
+
+# match in species richnesses
+for(x in seq(along=BenchData$node)){
+    currPat <- paste("([\\(\\),])", BenchData$node[x], "([\\(\\),])", sep="")
+    repPat <- paste("\\1", BenchData$SppRich[x], "\\2", sep="")
+    FuscoDi <- sub(currPat, repPat, FuscoDi)
+}
+
+FuscoDi <- sub(";", "*", FuscoDi) # switch ; to *
+# remove commas except the one between tips
+FuscoDi <- gsub("([0-9]),([0-9])", "\\1!\\2", FuscoDi)
+FuscoDi <- gsub(",", "", FuscoDi)
+FuscoDi <- gsub("!", ",", FuscoDi)
+cat(FuscoDi, file="BenchDiFusco.txt")
+
+# output files for analysis in Fusco
+FuscoPoly <- write.tree(BENCHPoly, multi.line=FALSE)
+FuscoPoly <- gsub(":[0-9\\.]+", "", FuscoPoly) # get rid of the branch lengths
+
+# match in species richnesses
+for(x in seq(along=BenchData$node)){
+    currPat <- paste("([\\(\\),])", BenchData$node[x], "([\\(\\),])", sep="")
+    repPat <- paste("\\1", BenchData$SppRich[x], "\\2", sep="")
+    FuscoPoly <- sub(currPat, repPat, FuscoPoly)
+}
+
+FuscoPoly <- sub(";", "*", FuscoPoly) # switch ; to *
+# remove commas except the one between tips
+FuscoPoly <- gsub("([0-9]),([0-9])", "\\1!\\2", FuscoPoly)
+FuscoPoly <- gsub(",", "", FuscoPoly)
+FuscoPoly <- gsub("!", ",", FuscoPoly)
+cat(FuscoPoly, file="BenchPolyFusco.txt")
